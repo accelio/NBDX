05aee8130ce1f2c4f5e1522f2ae3510da63a703b
Sagi Grimberg <sagig@mellanox.com>
2014-03-04 17:48:36 +0200

    autogen: Add backports facilities
    Signed-off-by: Sagi Grimberg <sagig@mellanox.com>

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,16 @@
-#! /bin/sh
+#! /bin/bash
 
+if [ "$(ls -A backports)" ]; then
+    git=`which git 2>/dev/null`
+    if [ -n "$git" ]; then
+        bport_branch=`git branch | grep -w backports`
+        if [ -n "$bport_branch" ]; then
+            git branch -D backports
+        fi
+        git checkout -b backports
+        git am backports/*
+    else
+        patch -p1 < backports/*
+    fi
+fi
 autoconf
