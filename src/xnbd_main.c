0dbd57f282d11ca6b82b2ac77b6babf034f16e3b
Sagi Grimberg <sagig@mellanox.com>
2014-03-04 15:09:47 +0200

    xnbd: Fix wrong err pointer check
    the check on PTR_ERR() return status should be IS_ERR()
    
    Signed-off-by: Sagi Grimberg <sagig@mellanox.com>

--- src/xnbd_main.c
+++ src/xnbd_main.c
@@ -568,8 +568,9 @@ static int xnbd_register_block_device(struct xnbd_file *xnbd_file)
 	xnbd_file->major = xnbd_major;
 
 	xnbd_file->queue = blk_mq_init_queue(&xnbd_mq_reg, xnbd_file);
-	if (PTR_ERR(xnbd_file->queue)) {
-		pr_err("%s: Failed to allocate blk queue\n", __func__);
+	if (IS_ERR(xnbd_file->queue)) {
+		pr_err("%s: Failed to allocate blk queue ret=%d\n",
+		       __func__, PTR_ERR(xnbd_file->queue));
 		return PTR_ERR(xnbd_file->queue);
 	}
 
